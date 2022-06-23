836f943c3689f8605748297dd043db5054f2894b
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-23 17:24:35 +0100

    broke: trying to import CL into the app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, crTheme } from '@comic-relief/component-library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <crTheme>
+        <App />
+      </crTheme>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
